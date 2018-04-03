d9e8e4c8e6087b2c99957f849bd6ef9d5dbc8d5d
Togose <tagodoy@uc.cl>
2018-04-03 11:17:16 -0300

    problemas con not found id solucionado

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,24 +1,24 @@
 class EntriesController < ApplicationController
   #before_action :authenticate_user!
-  before_action :define_entry
+  #before_action :define_entry
   before_action :define_header
   rescue_from ActiveRecord::RecordNotFound, :with => :not_found
   include EntriesHelper
 
   def index
     @entries = Entry.GetAll
-
     @entries.each do |entry|
       if entry.body.length >= 500
         last_space = entry.body[0..500].rindex(' ') - 1
         entry.body = entry.body[0..last_space]
       end
     end
-    output = {:entries => @entries}
+    output = @entries
     render json: output
   end
 
   def show
+    @entry = Entry.find(params[:id])
     if @entry
       render json: @entry
     else
@@ -35,8 +35,6 @@ class EntriesController < ApplicationController
       output = {:error => "Can't create an entry with that id"}
     else
       if @entry = Entry.create(entry_params)
-        #@entry = Entry.GetWithId(@entry.id)
-        #@comment = Comment.new(entry_id: @entry.id)
         render json: @entry, status: 201
       else
         output = {:error => "Creation has failed"}
@@ -46,6 +44,7 @@ class EntriesController < ApplicationController
   end
 
   def destroy
+    @entry = Entry.find(params[:id])
     if @entry
       @entry.destroy
       render status: 200
@@ -60,6 +59,7 @@ class EntriesController < ApplicationController
       output = {:error => "Bad Request: Id it's not editable"}
       render json: output, status: 400
     else
+      @entry = Entry.find(params[:id])
       if @entry
         if @entry.update(entry_params)
           render json: @entry
@@ -76,10 +76,6 @@ class EntriesController < ApplicationController
 
   private
 
-    def define_entry
-      @entry = Entry.find(params[:id])
-    end
-
     def not_found(error)
       render json: {:error => "/^not found$/gi"}.to_json, :status => 404
     end
@@ -89,7 +85,7 @@ class EntriesController < ApplicationController
     end
 
     def entry_params
-      params.permit(:title, :subtitle, :body, :created_at, :updated_at)
+      params.permit(:title, :subtitle, :body, :created_at, :updated_at, :id)
     end
 
 end
